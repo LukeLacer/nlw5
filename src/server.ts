2c0d2a5362a42444a49bcdfe84db2ebf7db572ef
LukeLacer <lucas_lacer@hotmail.com>
2021-04-21 07:51:04 -0300

    segunda aula

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express'
+import { routes } from './routes'
+
+import './database'
 
 const app = express()
 
-app.get('/', (request, response) => {
-    return response.json({ message: "API is ok" })
-})
+app.use(express.json())
 
-app.post('/new', (request, response) => {
-    console.log('POST - /new')
-    return response.json({ message: "User is saved!" })
-})
+app.use(routes)
 
 app.listen(5000, () => console.log('Server is running on port 5000'))
